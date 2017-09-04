2aae79a3614587cac2700c868af13ac0f6485f25
aanger258 <cosmin.g.musteata@gmail.com>
2017-09-04 14:25:11 +0300

    Second commit

--- app/City.php
+++ app/City.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     public $timestamps = false;
-    protected $table = 'cities';
+    protected $table = 'citiesaaaa';
+
 
     public function getRegion(){
         return $this->belongsTo('App\Zone','county_id','id')->select(array('name'));
